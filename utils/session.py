d4e4134f76cd0b3462cd770b2fffed04eb5d70b2
Michael Williams <mwill1003@gmail.com>
2025-03-31 14:14:46 -0500

    Fix syntax and cleanup session initializer

--- utils/session.py
+++ utils/session.py
@@ -14,4 +14,4 @@ def init_session_state():
 
     for key, value in defaults.items():
         if key not in st.session_state:
-            st.session_state[key] = value Placeholder for session management
+            st.session_state[key] = value
